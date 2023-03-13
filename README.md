0d25f81b0c9d8b2900fd7f09043871ceeedb865c
gturyz <gturyz@gmail.oom>
2023-03-13 16:56:03 +0100

    Init pocketbase package

---
+++ README.md
@@ -0,0 +1,11 @@
+# How to use
+
+Create .env file in the root directory of the project with the following content:
+
+```ts
+POCKETBASE_URL = "https://pocketbase.example.com";
+POCKETBASE_USERNAME = "username";
+POCKETBASE_PASSWORD = "password";
+
+PORT = 3000; // optional, default 3000
+```
--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import * as dotenv from "dotenv";
+const PocketBase = require('pocketbase/cjs');
+
+dotenv.config();
+
+const pb = new PocketBase(process.env.POCKETBASE_URL);
 
 // Initialization of Express
 const app: Express = express();
@@ -19,6 +25,13 @@ app.use(express.json());
 // First route
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World !");
+});
+
+// Get users collections from pocketbase
+app.get("/users", async (req: Request, res: Response) => {
+  await pb.admins.authWithPassword(process.env.POCKETBASE_USERNAME, process.env.POCKETBASE_PASSWORD);
+  const records = await pb.collection('users').getFullList();
+  res.send(records);
 });
 
 export = app;
